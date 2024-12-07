92a15fadef2e4812a9571deca7c3c0c44c8e940e
──「 ʟᴜᴄɪғᴇʀ 」── <nimovie09@gmail.com>
2024-12-07 21:20:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://bipinmfp07:OmodwqrRcvV6lrV4@cluster0.2t7so.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6899946963').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002345195670'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     PORT = environ.get('PORT', '8080')
